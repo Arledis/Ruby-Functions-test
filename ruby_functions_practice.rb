65ef7660391cf26c72df701900810a8381733fe1
Arledis <arleidysamaya_18@hotmail.com>
2019-05-07 19:08:36 +0100

    All functions done

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -32,9 +32,82 @@ def add_string_as_number(string_1, string_2)
   return string_1.to_i + string_2.to_i
 end
 
+def number_to_full_month_name(number)
+  result = case number
+    when 1
+      "January"
+    when 3
+      "March"
+    when 9
+      "September"
+  end
+    return result
+end
+
+def number_to_full_month_name(number)
+  result = case number
+    when 1
+      "January"
+    when 3
+      "March"
+    when 9
+      "September"
+  end
+    return result
+end
+
+def number_to_full_month_name(number)
+  result = case number
+    when 1
+      "January"
+    when 3
+      "March"
+    when 9
+      "September"
+  end
+    return result
+end
+
+def number_to_short_month_name(number)
+  result = case number
+    when 1
+      "Jan"
+    when 4
+      "Apr"
+    when 10
+      "Oct"
+  end
+    return result
+end
+
+def sumber_to_short_month_name(number)
+  result = case number
+    when 1
+      "Jan"
+    when 4
+      "Apr"
+    when 10
+      "Oct"
+  end
+    return result
+end
+
+def umber_to_short_month_name(number)
+  result = case number
+    when 1
+      "Jan"
+    when 4
+      "Apr"
+    when 10
+      "Oct"
+  end
+    return result
+end
+
 def volume_of_cube(num)
 return num**3
 end
+
 
 def volume_of_sphere(r)
 pi = 3.141592653
