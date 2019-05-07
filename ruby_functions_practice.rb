f3bb3848dd770749720b8d25024ceb14c8f4f19f
craiggough376 <craig_gough_376@hotmail.com>
2019-05-07 16:19:02 +0100

    Testing fuctions homework

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,37 @@
+def return_10()
+  return 10
+end
+
+def add(num_1, num_2)
+  return num_1 + num_2
+end
+
+def subtract(num_1, num_2)
+  return num_1 - num_2
+end
+
+
+def multiply(num_1, num_2)
+  return num_1 * num_2
+end
+
+def divide(num_1, num_2)
+  return num_1 / num_2
+end
+
+def length_of_string(length_of_string)
+  string = "A string of length 21"
+  return string.length
+end
+
+def join_string(string_1, string_2)
+  return string_1 + string_2
+end
+
+def add_string_as_number(string_1, string_2)
+  return string_1.to_i + string_2.to_i
+end
+
+def number_to_full_month_name_1(number_to_full_month_name)
+
+end
